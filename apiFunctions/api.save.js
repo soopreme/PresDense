fc3c5c75e8d39732dae858ed5577fe5ee63e2895
Unknown <eobrien16@ardscoil.ie>
2019-03-26 19:11:31 +0000

    Added ID count save
    bc fuck ups

--- apiFunctions/api.save.js
+++ apiFunctions/api.save.js
@@ -1,6 +1,6 @@
 const f = require('../functions');
 module.exports = function(req, res) {
-    f.saveNewData(__dirname + '/../profiles.json', f.profileArray)
+    f.saveNewData(__dirname + '/../profiles.json', {profileArray: f.profileArray, profileID: f.profileID})
     .then(() => res.sendStatus(200))
     .catch(err => res.sendStatus(500));
 }
--- functions/loadProfiles.js
+++ functions/loadProfiles.js
@@ -1,7 +1,8 @@
 const ext = require('./index')
 module.exports = file => new Promise((resolve, reject) => {
     try {
-        ext.profileArray = require(file);
+        ext.profileArray = require(file).profileArray;
+        ext.profileID = require(file).profileID;
         resolve();
     } catch (err) {
         reject(err);
