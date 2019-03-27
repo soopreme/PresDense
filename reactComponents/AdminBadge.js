37fd76b00a8c7e0021141d026f5cfaa4d1a908ca
Unknown <eobrien16@ardscoil.ie>
2019-03-27 20:53:16 +0000

    profile reactified

--- reactComponents/AdminBadge.js
+++ reactComponents/AdminBadge.js
@@ -2,8 +2,6 @@
 
 class AdminBadge extends React.Component {
     render() {
-        return((this.props.isAdmin)
-        ? <img style={{width: 40 + "px", height: 40 + "px"}} src="/res/adminIcon.png" />
-        : null);
+        return(<img style={{width: 40 + "px", height: 40 + "px"}} src="/res/adminIcon.png" />);
     }
 }
--- reactComponents/Profile.js
+++ reactComponents/Profile.js
@@ -11,18 +11,25 @@ class Profile extends React.Component {
         }
     }
     render() {
-        return ((this.state.isLoading)
+        const {error, profileData, isLoading} = this.state;
+        return ((isLoading)
         ? <div className="container"><Loading /></div>
+        : (error === 404)
+        ? <NotFound />
         : <div className="container">
-            <img id="pfp" src={this.state.profileData.picurl} />
-            <h2>{this.state.profileData.name}<AdminBadge isAdmin={this.state.profileData.isAdmin} /></h2>
-            <h4>{this.state.profileData.bio}</h4>
+            <img id="pfp" src={profileData.picurl} />
+            <h2>{profileData.name}</h2>{(profileData.isAdmin) ? <AdminBadge /> : null}
+            <h4>{profileData.bio}</h4>
+            <PlatformTable profileData={profileData} />
           </div>
         )
     }
     getData() {
         fetch("http://localhost:3010/api/id/" + this.props.id)
         .then(res => {
+            if(res.status === 404) {
+                this.setState({isLoading: false, profileData: null, error: 404});
+            }
             return res.json()
         })
         .then(json => {
@@ -34,7 +41,29 @@ class Profile extends React.Component {
     }
 }
 
+var viewProfileLink = "http://localhost:3010/View";
+var notFoundLink = "http://localhost:3010/404";
+
+function getID() {
+    if(window.location.href === viewProfileLink) {
+        return grabIDFromLocalStorage()
+    } else {
+        var untreatedIDArray = (window.location.toString()).split('/');
+        var Id = untreatedIDArray[untreatedIDArray.length-1];
+        return Id;
+    }
+}
+
+function grabIDFromLocalStorage() {
+    var id = localStorage.getItem('id');
+    if(id){
+        return id;
+    } else {
+        window.location = notFoundLink;
+    }
+}
+
 $(() => {
     const domContainer = document.querySelector('.root');
-    ReactDOM.render(<Profile id="0" />, domContainer);
+    ReactDOM.render(<Profile id={getID()} />, domContainer);
 })
